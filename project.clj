99951b91736d5f6548f0bfef32f7b063e8546b68
Bradford Cross <bradford@bradford-laptop.(none)>
2010-07-31 10:47:55 -0700

    forgot to add project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-http "0.0.1-SNAPSHOT"
+  :description "travelconnect"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+		 [org.apache.httpcomponents/httpclient "4.0.1"]
+		 [hiccup "0.2.6"]
+		 [org.danlarkin/clojure-json "1.1-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]
+                     [lein-clojars "0.5.0"]])
