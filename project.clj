606f29aa6d63086b0b3dc885c277c0fb32a8da18
Mark McGranaghan <mmcgrana@gmail.com>
2010-07-31 17:08:03 -0700

    normalize project.clj, upadate to most recent clojure versions

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject clj-http "0.0.1-SNAPSHOT"
-  :description "travelconnect"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :description
+    "A Clojure HTTP library wrapping the Apache HttpComponents client."
+  :dependencies
+    [[org.clojure/clojure "1.2.0-RC1"]
+     [org.clojure/clojure-contrib "1.2.0-RC1"]
 		 [org.apache.httpcomponents/httpclient "4.0.1"]
 		 [hiccup "0.2.6"]
 		 [org.danlarkin/clojure-json "1.1-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]
-                     [lein-clojars "0.5.0"]])
+  :dev-dependencies
+    [[swank-clojure "1.2.0"]
+     [lein-clojars "0.5.0"]])
