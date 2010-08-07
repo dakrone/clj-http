d115a4e06bc543554270186fd6ee69c26e76229a
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-06 17:11:49 -0700

    don't use broken lein-clojars

--- project.clj
+++ project.clj
@@ -2,12 +2,11 @@
   :description
     "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :dependencies
-    [[org.clojure/clojure "1.2.0-RC1"]
-     [org.clojure/clojure-contrib "1.2.0-RC1"]
+    [[org.clojure/clojure "1.2.0-RC2"]
+     [org.clojure/clojure-contrib "1.2.0-RC2"]
      [org.apache.httpcomponents/httpclient "4.0.1"]
      [commons-codec "1.3"]]
   :dev-dependencies
     [[swank-clojure "1.2.0"]
-     [lein-clojars "0.5.0"]
      [ring/ring-jetty-adapter "0.2.5"]
      [ring/ring-devel "0.2.5"]])
