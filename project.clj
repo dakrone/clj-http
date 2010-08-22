cb030affc08d4843752eb0535f94c544ec3ce741
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-22 14:45:00 -0700

    clojure 1.2.0 final

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description
     "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :dependencies
-    [[org.clojure/clojure "1.2.0-RC2"]
-     [org.clojure/clojure-contrib "1.2.0-RC2"]
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojure/clojure-contrib "1.2.0"]
      [org.apache.httpcomponents/httpclient "4.0.1"]
      [commons-codec "1.4"]
      [commons-io "1.4"]]
