f72caaceb30ec25202b04d330bd6be2ada2d1755
Mark McGranaghan <mmcgrana@gmail.com>
2010-12-13 08:02:51 -0800

    bump ring and httpclient deps

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :dependencies
     [[org.clojure/clojure "1.2.0"]
      [org.clojure/clojure-contrib "1.2.0"]
-     [org.apache.httpcomponents/httpclient "4.0.1"]
+     [org.apache.httpcomponents/httpclient "4.0.3"]
      [commons-codec "1.4"]
      [commons-io "1.4"]]
   :dev-dependencies
     [[swank-clojure "1.2.0"]
-     [ring/ring-jetty-adapter "0.2.5"]
-     [ring/ring-devel "0.2.5"]])
+     [ring/ring-jetty-adapter "0.3.5"]
+     [ring/ring-devel "0.3.5"]])
