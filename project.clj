7c5f24642cc3e7608dc646e19dbccfef7fff6f2f
Lee Hinman <lee@writequit.org>
2011-09-07 15:47:05 -0600

    update deps, bump to 0.2.0-SNAPSHOT, add missing clj-stacktrace dep

--- project.clj
+++ project.clj
@@ -1,16 +1,14 @@
-(defproject clj-http "0.1.3"
-  :description
-    "A Clojure HTTP library wrapping the Apache HttpComponents client."
-  :dependencies
-    [[org.clojure/clojure "1.2.0"]
-     [org.apache.httpcomponents/httpclient "4.0.3"]
-     [commons-codec "1.4"]
-     [commons-io "1.4"]]
-  :dev-dependencies
-    [[swank-clojure "1.2.0"]
-     [ring/ring-jetty-adapter "0.3.5"]
-     [ring/ring-devel "0.3.5"]
-     [robert/hooke "1.1.0"]]
+(defproject clj-http "0.2.0-SNAPSHOT"
+  :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.apache.httpcomponents/httpclient "4.1.2"]
+                 [commons-codec "1.5"]
+                 [commons-io "1.4"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]
+                     [ring/ring-jetty-adapter "0.3.11"]
+                     [ring/ring-devel "0.3.11"]
+                     [robert/hooke "1.1.2"]
+                     [clj-stacktrace "0.2.3"]]
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
                    :all (constantly true)})
