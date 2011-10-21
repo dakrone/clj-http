0c4fa4209e012824a90efe8abcf40c58868a72b5
nmishra <nmishra@symora.com>
2011-10-21 16:57:50 -0500

    migrating to 1.3

--- project.clj
+++ project.clj
@@ -2,15 +2,16 @@
   :description
     "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :dependencies
-    [[org.clojure/clojure "1.2.0"]
+    [[org.clojure/clojure "1.3.0"]
      [org.apache.httpcomponents/httpclient "4.0.3"]
      [commons-codec "1.4"]
      [commons-io "1.4"]]
   :dev-dependencies
-    [[swank-clojure "1.2.0"]
+    [[swank-clojure "1.3.2"]
      [ring/ring-jetty-adapter "0.3.5"]
      [ring/ring-devel "0.3.5"]
      [robert/hooke "1.1.0"]]
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
-                   :all (constantly true)})
+                   :all (constantly true)}
+  :main clj-http.core)
