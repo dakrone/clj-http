8bc77a9ff0b1856e0d8e86a8e35dbbdbf6e56593
Lee Hinman <lee@writequit.org>
2012-02-07 15:22:10 -0700

    start lein2 migration

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/dakrone/clj-http/"
   :repositories {"sona" "http://oss.sonatype.org/content/repositories/snapshots"}
   :warn-on-reflection false
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.httpcomponents/httpclient "4.1.2"]
                  [org.apache.httpcomponents/httpmime "4.1.2"]
@@ -10,23 +11,8 @@
                  [commons-io "2.1"]
                  [slingshot "0.10.2"]
                  [cheshire "2.2.2"]]
-  :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]
-                        [org.apache.httpcomponents/httpclient "4.1.2"]
-                        [org.apache.httpcomponents/httpmime "4.1.2"]
-                        [commons-codec "1.5"]
-                        [commons-io "2.1"]
-                        [slingshot "0.10.2"]
-                        [cheshire "2.2.2"]]
-               "1.4.0" [[org.clojure/clojure "1.4.0-beta3"]
-                        [org.apache.httpcomponents/httpclient "4.1.2"]
-                        [org.apache.httpcomponents/httpmime "4.1.2"]
-                        [commons-codec "1.5"]
-                        [commons-io "2.1"]
-                        [slingshot "0.10.2"]
-                        [cheshire "2.2.2"]]}
-  :dev-dependencies [[ring/ring-jetty-adapter "1.0.2"]
-                     [ring/ring-devel "1.0.2"]
-                     [lein-multi "1.1.0"]]
+  :profiles {"dev" {:dependencies [[ring/ring-jetty-adapter "1.0.2"]
+                                   [ring/ring-devel "1.0.2"]]}}
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
                    :all (constantly true)}
