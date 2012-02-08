63d4802a22b2725c4b5ac847fe4e101974ac1b38
Lee Hinman <lee@writequit.org>
2012-02-07 23:06:52 -0700

    multi-deps using profiles

--- project.clj
+++ project.clj
@@ -12,7 +12,9 @@
                  [slingshot "0.10.2"]
                  [cheshire "2.2.2"]]
   :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.0.2"]
-                                  [ring/ring-devel "1.0.2"]]}}
+                                  [ring/ring-devel "1.0.2"]]}
+             :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta1"]]}}
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
                    :all (constantly true)}
