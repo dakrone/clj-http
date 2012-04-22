2c0f98713f18bbf813b8d1d5b332a2867f979d9a
James Reeves <jreeves@weavejester.com>
2012-04-22 14:58:58 +0100

    Add parsing of link headers

---
+++ src/clj_http/links.clj
@@ -0,0 +1,53 @@
+(ns clj-http.links)
+
+(def ^:private quoted-string
+  #"\"((?:[^\"]|\\\")*)\"")
+
+(def ^:private token
+  #"([^,\";]*)")
+
+(def ^:private link-param
+  (re-pattern (str "(\\w+)=(?:" quoted-string "|" token ")")))
+
+(def ^:private uri-reference
+  #"<([^>]*)>")
+
+(def ^:private link-value
+  (re-pattern (str uri-reference "((?:\\s*;\\s*" link-param ")*)")))
+
+(def ^:private link-header
+  (re-pattern (str "(?:\\s*(" link-value ")\\s*,?\\s*)")))
+
+(defn read-link-params [params]
+  (into {}
+    (for [[_ name quot tok] (re-seq link-param params)]
+      [(keyword name) (or quot tok)])))
+
+(defn read-link-value [value]
+  (let [[_ uri params] (re-matches link-value value)
+        param-map      (read-link-params params)]
+    [(keyword (:rel param-map))
+     (-> param-map
+         (assoc :href uri)
+         (dissoc :rel))]))
+
+(defn read-link-headers [header]
+  (->> (re-seq link-header header)
+       (map second)
+       (map read-link-value)
+       (into {})))
+
+(defn wrap-links
+  "Add a :links key to the response map that contains parsed Link headers. The
+  links will be represented as a map, with the 'rel' value being the key. The
+  URI is placed under the 'href' key, to mimic the HTML link element.
+
+  e.g. Link: <http://example.com/page2.html>; rel=next; title=\"Page 2\"
+       => {:links {:next {:href \"http://example.com/page2.html\"
+                          :title \"Page 2\"}}}"
+  [client]
+  (fn [request]
+    (let [response (client request)]
+      (if-let [link-headers (get-in response [:headers "link"])]
+        (assoc response :links (read-link-headers link-headers))
+        response))))
