cba263978503f7ef7985f32904f8b7929cc9e9b9
Paul Schorfheide <paul@noveltyplant.com>
2012-06-07 12:52:32 -0400

    reset version info

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.noveltyplant/clj-http "0.4.3"
+(defproject clj-http "0.4.3-SNAPSHOT"
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :url "https://github.com/dakrone/clj-http/"
   :repositories {"sona" "http://oss.sonatype.org/content/repositories/snapshots"}
