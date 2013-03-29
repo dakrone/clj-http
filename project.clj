a17079a50b7b58bd9b56de78462579a0a5373f6a
Lee Hinman <lee@writequit.org>
2013-03-29 10:40:40 -0600

    bump deps to latest versions

--- project.clj
+++ project.clj
@@ -8,17 +8,17 @@
   :warn-on-reflection false
   :min-lein-version "2.0.0"
   :dependencies [[org.apache.httpcomponents/httpcore "4.2.3"]
-                 [org.apache.httpcomponents/httpclient "4.2.2"]
-                 [org.apache.httpcomponents/httpmime "4.2.2"]
-                 [commons-codec "1.6"]
+                 [org.apache.httpcomponents/httpclient "4.2.3"]
+                 [org.apache.httpcomponents/httpmime "4.2.3"]
+                 [commons-codec "1.7"]
                  [commons-io "2.4"]
                  [slingshot "0.10.3"]
                  [cheshire "5.0.2"]
                  [crouton "0.1.1"]
                  [org.clojure/tools.reader "0.7.3"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [ring/ring-jetty-adapter "1.1.0"]
-                                  [ring/ring-devel "1.1.0"]]}
+                                  [ring/ring-jetty-adapter "1.1.8"]
+                                  [ring/ring-devel "1.1.8"]]}
              :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
