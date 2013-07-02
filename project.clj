35bf3afe08918bd90bd253c69c3e101e104ed949
Lee Hinman <lee@writequit.org>
2013-07-02 11:23:31 -0600

    bump reader and cheshire deps, remove unneeded repo, turn on warn-on-reflection

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :repositories {"sona" "http://oss.sonatype.org/content/repositories/snapshots"}
-  :warn-on-reflection false
+  :global-vars {*warn-on-reflection* true}
   :min-lein-version "2.0.0"
   :dependencies [[org.apache.httpcomponents/httpcore "4.2.4"]
                  [org.apache.httpcomponents/httpclient "4.2.5"]
@@ -13,9 +12,9 @@
                  [commons-codec "1.7"]
                  [commons-io "2.4"]
                  [slingshot "0.10.3" :exclusions [org.clojure/clojure]]
-                 [cheshire "5.1.1"]
+                 [cheshire "5.2.0"]
                  [crouton "0.1.1" :exclusions [org.clojure/clojure]]
-                 [org.clojure/tools.reader "0.7.3"
+                 [org.clojure/tools.reader "0.7.4"
                   :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
                                   [ring/ring-jetty-adapter "1.1.8"]
