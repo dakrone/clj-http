9180e05a97c76b522687d15cf6d74772a359edc5
Lee Hinman <lee@writequit.org>
2014-06-16 10:54:58 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -8,20 +8,20 @@
   :min-lein-version "2.0.0"
   :exclusions [org.clojure/clojure]
   :dependencies [[org.apache.httpcomponents/httpcore "4.3.2"]
-                 [org.apache.httpcomponents/httpclient "4.3.3"]
-                 [org.apache.httpcomponents/httpmime "4.3.3"]
+                 [org.apache.httpcomponents/httpclient "4.3.4"]
+                 [org.apache.httpcomponents/httpmime "4.3.4"]
                  [commons-codec "1.9"]
                  [commons-io "2.4"]
                  [slingshot "0.10.3"]
                  [cheshire "5.3.1"]
                  [crouton "0.1.2"]
-                 [org.clojure/tools.reader "0.8.4"]
+                 [org.clojure/tools.reader "0.8.5"]
                  [potemkin "0.3.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/tools.logging "0.2.6"]
+                                  [org.clojure/tools.logging "0.3.0"]
                                   [log4j "1.2.17"]
-                                  [ring/ring-jetty-adapter "1.2.2"]
-                                  [ring/ring-devel "1.2.2"]]}
+                                  [ring/ring-jetty-adapter "1.3.0"]
+                                  [ring/ring-devel "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev"]}
