3d151d58fe6b9b763bfa8c547e1055d9782d68c1
Lee Hinman <lee@writequit.org>
2014-08-11 13:51:32 +0200

    drop support for clojure 1.4.0, start testing 1.7.0-alpha1

--- project.clj
+++ project.clj
@@ -17,15 +17,16 @@
                  [cheshire "5.3.1"]
                  [crouton "0.1.2"]
                  [org.clojure/tools.reader "0.8.5"]
-                 [potemkin "0.3.4"]]
+                 [potemkin "0.3.8"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [org.clojure/tools.logging "0.3.0"]
                                   [log4j "1.2.17"]
                                   [ring/ring-jetty-adapter "1.3.0"]
                                   [ring/ring-devel "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev"]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha1"]]}}
+  :aliases {"all" ["with-profile" "dev,1.4:dev,1.5:dev:dev,1.7"]}
   :plugins [[codox "0.6.4"]]
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
