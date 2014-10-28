7b589d308b16d6f5b966ca0e4c086ca09facf145
Lee Hinman <lee@writequit.org>
2014-10-28 20:34:39 +0100

    Release 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-http "1.0.1-SNAPSHOT"
+(defproject clj-http "1.0.1"
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :url "https://github.com/dakrone/clj-http/"
   :license {:name "The MIT License"
