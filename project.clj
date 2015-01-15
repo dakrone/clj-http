f33befb00cb70017d456b9f41fc62b075826331d
Lee Hinman <lee@writequit.org>
2015-01-15 10:21:25 +0100

    Bump all the dependencies

--- project.clj
+++ project.clj
@@ -7,24 +7,24 @@
   :global-vars {*warn-on-reflection* true}
   :min-lein-version "2.0.0"
   :exclusions [org.clojure/clojure]
-  :dependencies [[org.apache.httpcomponents/httpcore "4.3.3"]
-                 [org.apache.httpcomponents/httpclient "4.3.5"]
-                 [org.apache.httpcomponents/httpmime "4.3.5"]
-                 [commons-codec "1.9"]
+  :dependencies [[org.apache.httpcomponents/httpcore "4.4"]
+                 [org.apache.httpcomponents/httpclient "4.3.6"]
+                 [org.apache.httpcomponents/httpmime "4.3.6"]
+                 [commons-codec "1.10"]
                  [commons-io "2.4"]
                  [com.cognitect/transit-clj "0.8.259"]
-                 [slingshot "0.10.3"]
-                 [cheshire "5.3.1"]
+                 [slingshot "0.12.1"]
+                 [cheshire "5.4.0"]
                  [crouton "0.1.2"]
-                 [org.clojure/tools.reader "0.8.10"]
-                 [potemkin "0.3.8"]]
+                 [org.clojure/tools.reader "0.8.13"]
+                 [potemkin "0.3.11"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [org.clojure/tools.logging "0.3.1"]
                                   [log4j "1.2.17"]
-                                  [ring/ring-jetty-adapter "1.3.1"]
-                                  [ring/ring-devel "1.3.1"]]}
+                                  [ring/ring-jetty-adapter "1.3.2"]
+                                  [ring/ring-devel "1.3.2"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
   :aliases {"all" ["with-profile" "dev,1.5:dev:dev,1.7"]}
   :plugins [[codox "0.6.4"]]
   :test-selectors {:default  #(not (:integration %))
