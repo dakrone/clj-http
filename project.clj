969aab8476bf6f636dcd6d08e1b720727449a511
Lee Hinman <lee@writequit.org>
2015-04-22 23:27:08 -0600

    Bump to 1.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-http "1.1.1"
+(defproject clj-http "1.1.2-SNAPSHOT"
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :url "https://github.com/dakrone/clj-http/"
   :license {:name "The MIT License"
