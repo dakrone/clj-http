1dfed8146eccee6b81257fd18f2c781808f3aab4
Lee Hinman <lee@writequit.org>
2015-05-24 08:40:31 -0600

    Missed the transit dep when making dependencies optional

--- project.clj
+++ project.clj
@@ -12,13 +12,13 @@
                  [org.apache.httpcomponents/httpmime "4.4.1"]
                  [commons-codec "1.10"]
                  [commons-io "2.4"]
-                 [com.cognitect/transit-clj "0.8.271"]
                  [slingshot "0.12.2"]
                  [potemkin "0.3.13"]]
   :profiles {:dev {:dependencies [;; optional deps
                                   [cheshire "5.4.0"]
                                   [crouton "0.1.2"]
                                   [org.clojure/tools.reader "0.9.2"]
+                                  [com.cognitect/transit-clj "0.8.271"]
                                   [ring/ring-codec "1.0.0"]
                                   ;; other (testing) deps
                                   [org.clojure/clojure "1.6.0"]
