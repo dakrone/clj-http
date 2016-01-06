66fe9103db7da3b3745e686ee4e5e96935172bf1
Mansur Ziiatdinov <gltronred@gmail.com>
2016-01-06 14:57:10 +0300

    Version 2.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdakazan/clj-http "2.0.3-SNAPSHOT"
+(defproject lambdakazan/clj-http "2.0.3"
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :url "https://github.com/dakrone/clj-http/"
   :license {:name "The MIT License"
