5f24e7b86644324840d25d490ec7c078e2245804
Lee Hinman <lee@writequit.org>
2016-04-09 21:28:53 -0600

    Bump dependencies

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
   :min-lein-version "2.0.0"
   :exclusions [org.clojure/clojure]
   :dependencies [[org.apache.httpcomponents/httpcore "4.4.4"]
-                 [org.apache.httpcomponents/httpclient "4.5.1"]
-                 [org.apache.httpcomponents/httpmime "4.5.1"]
+                 [org.apache.httpcomponents/httpclient "4.5.2"]
+                 [org.apache.httpcomponents/httpmime "4.5.2"]
                  [commons-codec "1.10"]
                  [commons-io "2.4"]
                  [slingshot "0.12.2"]
                  [potemkin "0.4.3"]]
   :profiles {:dev {:dependencies [;; optional deps
-                                  [cheshire "5.5.0"]
+                                  [cheshire "5.6.0"]
                                   [crouton "0.1.2"]
                                   [org.clojure/tools.reader "0.10.0"]
                                   [com.cognitect/transit-clj "0.8.285"]
