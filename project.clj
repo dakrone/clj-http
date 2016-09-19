7bcf8fd5ea71545817db3b4fad1e9294fa11f9e1
Lee Hinman <lee@writequit.org>
2016-09-19 13:14:00 -0600

    Bump dependencies for 2.x

--- project.clj
+++ project.clj
@@ -7,25 +7,25 @@
   :global-vars {*warn-on-reflection* false}
   :min-lein-version "2.0.0"
   :exclusions [org.clojure/clojure]
-  :dependencies [[org.apache.httpcomponents/httpcore "4.4.4"]
-                 [org.apache.httpcomponents/httpclient "4.5.1"]
-                 [org.apache.httpcomponents/httpmime "4.5.1"]
+  :dependencies [[org.apache.httpcomponents/httpcore "4.4.5"]
+                 [org.apache.httpcomponents/httpclient "4.5.2"]
+                 [org.apache.httpcomponents/httpmime "4.5.2"]
                  [commons-codec "1.10"]
-                 [commons-io "2.4"]
+                 [commons-io "2.5"]
                  [slingshot "0.12.2"]
                  [potemkin "0.4.3"]]
   :profiles {:dev {:dependencies [;; optional deps
-                                  [cheshire "5.5.0"]
+                                  [cheshire "5.6.3"]
                                   [crouton "0.1.2"]
                                   [org.clojure/tools.reader "0.10.0"]
-                                  [com.cognitect/transit-clj "0.8.285"]
-                                  [ring/ring-codec "1.0.0"]
+                                  [com.cognitect/transit-clj "0.8.288"]
+                                  [ring/ring-codec "1.0.1"]
                                   ;; other (testing) deps
                                   [org.clojure/clojure "1.8.0"]
                                   [org.clojure/tools.logging "0.3.1"]
                                   [log4j "1.2.17"]
-                                  [ring/ring-jetty-adapter "1.4.0"]
-                                  [ring/ring-devel "1.4.0"]]}
+                                  [ring/ring-jetty-adapter "1.5.0"]
+                                  [ring/ring-devel "1.5.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :aliases {"all" ["with-profile" "dev,1.6:dev,1.7:dev"]}
