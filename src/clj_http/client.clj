47a7762ed42e1d772e51a6f5bdaa61b436b54cb3
Lee Hinman <dakrone@users.noreply.github.com>
2018-02-12 19:48:50 -0700

    Add options for how to flatten/encode form and query parameters (#433)
    * Add options for how to flatten/encode form and query parameters
    
    This adds the following new parameters:
    
    - `:ignore-nested-query-string` :: Do not handle nested query parameters specially, treat them as
         the exact text they come in as. Defaults to *false*.
    - `:flatten-nested-form-params` :: Flatten nested (map within a map) `:form-params` before encoding
         it as the body. Defaults to *false*, meaning form params are encoded only
         `x-www-form-urlencoded`.
    - `:flatten-nested-keys` :: An advanced way of specifying which keys having nested maps should be
         flattened. A middleware function checks the previous two options
         (`:ignore-nested-query-string` and `:flatten-nested-form-params`) and modifies this to be the
         list that will be flattened.
    
    Resolves #427
    
    * Add a test for the new middleware
    
    * Throw IllegalArgumentException when multiple options are specified

--- src/clj_http/client.clj
+++ src/clj_http/client.clj
@@ -863,13 +863,12 @@
     request))
 
 (defn- nest-params-request
-  [{:keys [content-type] :as req}]
-  (if (or (nil? content-type)
-          (= content-type :x-www-form-urlencoded))
+  [{:keys [flatten-nested-keys] :as req}]
+  (if (seq flatten-nested-keys)
     (reduce
      nest-params
      req
-     [:query-params :form-params])
+     flatten-nested-keys)
     req))
 
 (defn wrap-nested-params
@@ -880,6 +879,33 @@
      (client (nest-params-request req)))
     ([req respond raise]
      (client (nest-params-request req) respond raise))))
+
+(defn- nested-keys-to-flatten
+  [{:keys [flatten-nested-keys] :as req}]
+  (when (and (not (nil? (opt req :ignore-nested-query-string)))
+             (not (nil? (opt req :flatten-nested-form-params)))
+             flatten-nested-keys)
+    (throw (IllegalArgumentException.
+            (str "only :flatten-nested-keys or :ignore-nested-query-string/"
+                 ":flatten-nested-keys may be specified, not both"))))
+  (let [iqs-key (when-not (opt req :ignore-nested-query-string) :query-params)
+        ifp-key (when (opt req :flatten-nested-form-params) :form-params)]
+    (or flatten-nested-keys
+        (remove nil? (list iqs-key ifp-key)))))
+
+(defn wrap-flatten-nested-params
+  "Middleware wrapping options for whether or not to flatten `:query-params` and
+  `:form-params`. Modifies the request by adding a `:flatten-nested-keys`
+  sequence of the nested keys that will be flattened."
+  [client]
+  (fn
+    ([req]
+     (client
+      (assoc req :flatten-nested-keys (nested-keys-to-flatten req))))
+    ([req respond raise]
+     (client
+      (assoc req :flatten-nested-keys (nested-keys-to-flatten req))
+      respond raise))))
 
 (defn- url-request
   [req]
@@ -1014,6 +1040,7 @@
    wrap-content-type
    wrap-form-params
    wrap-nested-params
+   wrap-flatten-nested-params
    wrap-method
    wrap-cookies
    wrap-links
