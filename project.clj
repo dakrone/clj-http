44cd40ff5b9c87575ab9faa00838736e06eec221
Lee Hinman <lee@writequit.org>
2018-04-24 21:25:53 -0600

    Drop support for clojure 1.6

--- project.clj
+++ project.clj
@@ -40,10 +40,9 @@
                              [jonase/eastwood "0.2.5"]
                              [lein-kibit "0.1.5"]
                              [lein-nvd "0.5.2"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
-  :aliases {"all" ["with-profile" "dev,1.6:dev,1.7:dev,1.8:dev"]}
+  :aliases {"all" ["with-profile" "dev,1.7:dev,1.8:dev"]}
   :plugins [[codox "0.6.4"]]
   :test-selectors {:default  #(not (:integration %))
                    :integration :integration
