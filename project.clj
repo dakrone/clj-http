9703a4c9a38f617cb0d58845f3d4af4b345eb1c9
Lee Hinman <lee@writequit.org>
2018-08-01 10:16:36 -0600

    Bump to 3.9.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-http "3.9.1"
+(defproject clj-http "3.9.2-SNAPSHOT"
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :url "https://github.com/dakrone/clj-http/"
   :license {:name "The MIT License"
