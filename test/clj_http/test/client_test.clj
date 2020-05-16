c6729a47879f360def3b50932125bd500bcaef22
Raymond Huang <rymndhng@gmail.com>
2020-05-16 00:14:46 -0700

    Add option :async-future to return a future-wrapped response
    The existing :async option returns a future of org.apache.http.HttpResponse,
    which is not the ideal type for Clojurists.
    
    This new option :async-future returns a future of a clj-http response.

--- test/clj_http/test/client_test.clj
+++ test/clj_http/test/client_test.clj
@@ -167,6 +167,45 @@
             (is (= params (read-fn (:body @resp))))
             (is (not (realized? exception)))))))))
 
+(deftest ^:integration roundtrip-async-future
+  (run-server)
+  ;; roundtrip with scheme as a keyword
+  (let [resp (request {:uri "/get" :method :get
+                       :async-future? true})]
+    (is (= 200 (:status @resp)))
+    (is (= "close" (get-in @resp [:headers "connection"])))
+    (is (= "get" (:body @resp))))
+  ;; roundtrip with scheme as a string
+  (let [resp (request {:uri "/get" :method :get
+                       :scheme "http"
+                       :async-future? true})]
+    (is (= 200 (:status @resp)))
+    (is (= "close" (get-in @resp [:headers "connection"])))
+    (is (= "get" (:body @resp))))
+  ;; error handling
+  (let [resp (request {:uri "/error" :method :get
+                       :async-future? true})]
+    (is (thrown? java.util.concurrent.ExecutionException
+          @resp)))
+
+  (let [params {:a "1" :b "2"}]
+    (doseq [[content-type read-fn]
+            [[nil (comp parse-form-params slurp)]
+             [:x-www-form-urlencoded (comp parse-form-params slurp)]
+             [:edn (comp read-string slurp)]
+             [:transit+json #(client/parse-transit % :json)]
+             [:transit+msgpack #(client/parse-transit % :msgpack)]]]
+      (let [resp (request {:uri "/post"
+                           :as :stream
+                           :method :post
+                           :content-type content-type
+                           :flatten-nested-keys []
+                           :form-params params
+                           :async-future? true})]
+        (is (= 200 (:status @resp)))
+        (is (= "close" (get-in @resp [:headers "connection"])))
+        (is (= params (read-fn (:body @resp))))))))
+
 (def ^:dynamic *test-dynamic-var* nil)
 
 (deftest ^:integration async-preserves-dynamic-variable-bindings
