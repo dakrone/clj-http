aee13b4f544e83e4eb6014d6d1f9178676899f13
Raymond Huang <rymndhng@gmail.com>
2020-08-04 20:52:14 -0700

    Version 3.10.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-http "3.10.2"
+(defproject clj-http "3.10.3-SNAPSHOT"
   :description "A Clojure HTTP library wrapping the Apache HttpComponents client."
   :url "https://github.com/dakrone/clj-http/"
   :license {:name "The MIT License"
