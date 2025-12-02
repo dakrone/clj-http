73b12627f210a76e0df8b99d50bd2ba4f67942d9
David Harrigan <dharrigan@users.noreply.github.com>
2025-12-02 17:20:58 +0000

    Use clj-commons.slingshot (#659)
    Sadly, the maintainer of the original slingshot has passed away.
    
    The slingshot library has been adopted into the clj-commons repository
    and will be maintained under a different set of coordinates going
    forward.
    
    fixes #658
    
    -=david=-

--- project.clj
+++ project.clj
@@ -11,9 +11,9 @@
                  [org.apache.httpcomponents/httpclient-cache "4.5.14"]
                  [org.apache.httpcomponents/httpasyncclient "4.1.5"]
                  [org.apache.httpcomponents/httpmime "4.5.14"]
+                 [org.clj-commons/slingshot "0.13.0"]
                  [commons-codec "1.16.1"]
                  [commons-io "2.16.1"]
-                 [slingshot "0.12.2"]
                  [potemkin "0.4.7"]]
   :resource-paths ["resources"]
   :profiles {:dev {:dependencies [;; optional deps
